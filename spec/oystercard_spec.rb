2876e55af3b2f5c4bb018ffd6aec8e07b0034d83
NikolayStoyanov87 <bushon87@gmail.com>
2018-10-08 15:05:02 +0100

    Initializad balance 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
 describe Oystercard do
-  
+describe '#balance' do
+  it 'has an initial balance 0' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
+end
+
 end
