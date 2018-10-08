7a11bf32d37ae3ff45050b4ef8a13899278b394f
rbbri <robin.s.johnson@gmail.com>
2018-10-08 15:10:17 +0100

    added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,5 +6,11 @@ describe '#balance' do
     expect(oystercard.balance).to eq 0
   end
 end
-
+describe '#top_up' do
+  it 'adds money to the card' do
+    oystercard = Oystercard.new
+    oystercard.top_up(5)
+    expect(oystercard.balance).to eq 5
+  end
+end
 end
