78309ef079a8ddcc255a2b845b2b6e789fd5333c
rbbri <robin.s.johnson@gmail.com>
2018-10-08 16:06:59 +0100

    Added in_journey, touch_in and touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,7 @@ class Oystercard
   attr_reader :balance
   def initialize
     @balance = 0
+    @in_use = false
   end
 
   def top_up(amount)
@@ -14,10 +15,25 @@ class Oystercard
     @balance -= amount
   end
 
+  def in_journey?
+    @in_use
+  end
+
+  def touch_in
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+
   private
 
   def max?
     @balance >= MAXIMUM_BALANCE
   end
+
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,35 +1,57 @@
 require 'oystercard'
 describe Oystercard do
+    it { is_expected.to respond_to(:in_journey?) }
+
+
 describe '#balance' do
   it 'has an initial balance 0' do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 end
 describe '#top_up' do
   it 'adds money to the card' do
-    oystercard = Oystercard.new
-    oystercard.top_up(5)
-    expect(oystercard.balance).to eq 5
+    subject.top_up(5)
+    expect(subject.balance).to eq 5
   end
 
 describe '#deduct' do
   it 'subtracts money from the card' do
-    oystercard = Oystercard.new
-    oystercard.top_up(10)
-    oystercard.deduct(5)
-    expect(oystercard.balance).to eq 5        
+    subject.top_up(10)
+    subject.deduct(5)
+    expect(subject.balance).to eq 5
+  end
+end
+
+describe '#in_journey?' do
+ it 'is initially not in a journey' do
+   expect(subject.in_journey?).to eq false
+ end
+
+end
+
+describe '#touch_in' do
+  it 'starts a journey' do
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
+  end
+end
+
+describe '#touch_out' do
+  it 'ends the journey' do
+    subject.touch_out
+    expect(subject.in_journey?).to eq false
   end
 end
 
 context 'maximum balance exceeded' do
   it 'raises an error' do
-    oystercard = Oystercard.new
     maximum_balance = Oystercard::MAXIMUM_BALANCE
-    oystercard.top_up(90)
-    expect {oystercard.top_up(1)}.to raise_error("Maximum balance is £#{maximum_balance}")
+    subject.top_up(90)
+    expect {subject.top_up(1)}.to raise_error("Maximum balance is £#{maximum_balance}")
   end
-
 end
+
+
+
 end
 end
